cf412f144c2522b4fc0684dcca02b392ad9f2d48
houyafeng <houyafeng89@gmail.com>
2013-11-08 09:35:25 +0800

    add 富汇易达原始凭证金额分转元

--- lib/ZixWeb/Utils.pm
+++ lib/ZixWeb/Utils.pm
@@ -427,10 +427,9 @@ sub _transform {
 		zg_bfee_1_back zg_bfee_2_back zg_bfee_3_back bfee_1_back
 		cwwf_bfee_1 cwwf_bfee_2 cwwf_bfee_3 cc_bfee_1 cc_bfee_2 cc_bfee_3
 		cc_bfee_1_back cc_bfee_2_back cc_bfee_3_back in_cost rb_cwwf_bfee
-		rb_cwwf_bfee_back bfee_4 bfee_5 cwwf_bfee_4 cwwf_bfee_5 zg_bfee_4 zg_bfee_5  back_amt
-        income_amt
-        entrust_amt
-        loss_amt
+		rb_cwwf_bfee_back bfee_4 bfee_5 cwwf_bfee_4 cwwf_bfee_5 zg_bfee_4 zg_bfee_5  
+
+        tx_amt cj_amt chtx_amt mtx_amt income_amt back_amt entrust_amt yyws_amt ls_amt
 		/
 	  )
 	{
